a8c910785a93b242d8a9cafe6b4a0f1984eb6a65
Bakr Hawsawi <bakrhawsawi4@gmail.com>
2023-08-09 23:10:52 +0300

    update HW14

--- assignment---14/src/main/java/com/example/assignment14/Model/Employees.java
+++ assignment---14/src/main/java/com/example/assignment14/Model/Employees.java
@@ -11,32 +11,33 @@ import java.util.Date;
 @AllArgsConstructor
 public class Employees {
 
-    @NotEmpty(message = "should not be empty")
-    @Size(min =2,message = "should be more than 2")
+    @NotEmpty(message = "ID should not be empty")
+    @Size(min =2,message = "ID should be more than 2")
     private String id;
 
-    @NotEmpty(message = "should not be empty")
-    @Size(min = 4,message = "should be more than 4")
+    @NotEmpty(message = "Name should not be empty")
+    @Size(min = 4,message = "Name should be more than 4")
     private String name;
 
-    @NotNull(message = "cant be null")
-    @Min(25)
+    @NotNull(message = "Age cant be null")
+    @Min(value = 25,message = "Age should be more than 25")
     private int age;
 
-    @NotEmpty(message = "cant be empty")
-    @Pattern(regexp = "supervisor|coordinator")
+    @NotEmpty(message = "Position cant be empty")
+    @Pattern(regexp = "supervisor|coordinator",message = "Position should be supervisor OR coordinator")
     private String position;
 
-    @NotNull
-    private boolean onLeave=false;
+    @NotNull(message = "OnLeave cannot be empty")
+    @AssertFalse(message = "onLeave should be false")
+    private boolean onLeave;
 
     @Positive(message = "invalid year")
     @NotNull(message = "employmentYear cannot be empty")
     @Digits(integer = 4, fraction = 0, message = "employmentYear must be a 4-digit number")
     private int employmentYear;
 
-    @Positive(message = "annualLeave should not be null")
-    @NotNull(message = "cant be empty")
+    @NotNull(message = "annualLeave cant be empty")
+    @Min(0)
     private int annualLeave;
 
 
